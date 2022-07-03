3856df7541c5f5a833a43f1b001637ca8a53cc91
hesung <hsjo6864@gmail.com>
2022-07-03 18:39:49 +0900

    2022.07.03 스프링 공부 인프런 스프링 핵심원리 - 기본편 순수 자바로 DI 구현했던 내용을 스프링으로 구현 - ApplicationContext 로 DI 구현 - ApplicationContext를 활용하는 여러 테스트 케이스 작성

---
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,56 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApplicationContextBasicFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName(){
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입으로만 조회")
+    void findBeanByType(){
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByType2(){
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+
+//        System.out.println("memberService = " + memberService);
+//        System.out.println("memberService.getClass() = " + memberService.getClass());
+
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회 X")
+    void findBeanNameByX(){
+//        ac.getBean("xxx", MemberService.class);
+
+        //NoSuchBeanDefinitionException 예외가 발생하면 성공하는 테스트트
+       assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxx", MemberService.class));
+    }
+}
