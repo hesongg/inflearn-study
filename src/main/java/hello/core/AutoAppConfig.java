d88fef5aa6cc050c8e5888900032de570e11752c
hesung <hsjo6864@gmail.com>
2022-07-05 00:29:48 +0900

    2022.07.04 스프링 공부 인프런 스프링 핵심원리 - 기본편 - 컴포넌트 스캔의 필터, 필터 타입 - 빈 등록 충돌 발생 시?

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core",
         //이제부터 AutoAppConfig 테스트를 위해 AppConfig bean등록을 제외함
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
