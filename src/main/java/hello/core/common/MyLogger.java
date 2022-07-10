f9da27aed46795d100db08f69485bcc9860e7ebb
hesung <hsjo6864@gmail.com>
2022-07-10 21:17:35 +0900

    2022.07.10 스프링 공부 인프런 스프링 핵심원리 - 기본편 - request scope 빈 코드를 provider 대신 프록시모드로 사용하도록 수정

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,8 @@
 package hello.core.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +10,7 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request")
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class MyLogger {
 
     private String uuid;
